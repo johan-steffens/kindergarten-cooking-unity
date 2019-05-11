a8518b27935c0a683f4f4ae103bfdfce054570a3
= <=>
2019-05-11 14:24:49 +0200

    feat: added plate hover state

--- Assets/Scripts/Plate/PlateController.cs
+++ Assets/Scripts/Plate/PlateController.cs
@@ -1,20 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlateController : MonoBehaviour
 {
 
     public List<Ingredient> addedIngredients = new List<Ingredient>();
-
+    public Collider2D hoverCollider;
+    public Sprite normalSprite;
+    public Sprite hoverSprite;
     public Recipe recipe;
-    private RecipeCard card;
 
+    private SpriteRenderer spriteRenderer;
+    private RecipeCard card;
     private PlateMovement movement;
+
+    private bool hovering = false;
 
     void Start()
     {
         movement = GetComponent<PlateMovement>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // Check if mouse is hovering over plate
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(ray.origin, ray.direction, Mathf.Infinity);
+        if (! hovering && hits.Where(hit => { return hit.collider == hoverCollider; }).Count() >= 1)
+        {
+            hovering = true;
+            SetSprite(hovering);
+        } else if(hovering && hits.Where(hit => { return hit.collider == hoverCollider; }).Count() == 0)
+        {
+            hovering = false;
+            SetSprite(hovering);
+        }
+    }
+
+    public void NotifyHover(bool hovering)
+    {
+        SetSprite(hovering);
+    }
+
+    private void SetSprite(bool hovering)
+    {
+        spriteRenderer.sprite = hovering ? hoverSprite : normalSprite;
     }
 
     public void AddIngredient(Ingredient ingredient)
@@ -45,4 +78,5 @@ public class PlateController : MonoBehaviour
     {
         this.recipe = recipe;
     }
+
 }
